78c27870c767b8d052ba4a006b3b6f78b00b11c9
MarkusEck <40233647+MarkusEck@users.noreply.github.com>
2018-06-13 15:16:03 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qQJO-Um66SkOCRl7cRr_1wTm200DhrB3RKmr1_vcRT4/edit#gid=0';
